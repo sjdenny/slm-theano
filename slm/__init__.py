dee1f8fea2a2e61802aca453b5f4afa857265995
Samuel Denny <samueljdenny@gmail.com>
2015-04-22 18:02:25 +0100

    Developing example 1

--- slm/__init__.py
+++ slm/__init__.py
@@ -1,4 +1,4 @@
-import ...
+import theano
 
 # Take code from March expts.ipynb
 
@@ -50,3 +50,77 @@ class slmOptimisation(object):
             self.intensity_calc = theano.function(['S_i', 'S_r'], self.E_out_2)
         
         return self.intensity_calc([self.profile_s_r, self.profile_s_i])
+
+
+
+
+class InverseFourierOp(theano.Op):
+    __props__ = ()
+    
+    def make_node(self, xr, xi):
+        # check that the theano version has support for __props__
+        assert hasattr(self, '_props')
+        xr = T.as_tensor_variable(xr)
+        xi = T.as_tensor_variable(xi)
+        
+        return theano.Apply(self, [xr, xi], [xr.type(), xr.type()])
+    
+    def perform(self, node, inputs, output_storage):
+        x = inputs[0] + 1j*inputs[1]
+        nx, ny = inputs[0].shape
+        z_r = output_storage[0]
+        z_i = output_storage[1]
+        s = np.fft.ifft2(x) * (nx*ny)
+        z_r[0] = np.real(s)
+        z_i[0] = np.imag(s)
+
+
+class FourierOp(theano.Op):
+    __props__ = ()
+    
+    def make_node(self, xr, xi):
+        # check that the theano version has support for __props__
+        assert hasattr(self, '_props')
+        xr = T.as_tensor_variable(xr)
+        xi = T.as_tensor_variable(xi)
+        
+        return theano.Apply(self, [xr, xi], [xr.type(), xr.type()])
+    
+    def perform(self, node, inputs, output_storage):
+        x = inputs[0] + 1j*inputs[1]
+        z_r = output_storage[0]
+        z_i = output_storage[1]
+        s = np.fft.fft2(x)  # has "1" normalisation
+        z_r[0] = np.real(s)
+        z_i[0] = np.imag(s)
+        
+    def grad(self, inputs, output_gradients):
+        """
+        From the docs:
+        If an Op has a single vector-valued output y and a single vector-valued input x,
+        then the grad method will be passed x and a second vector z. Define J to be the 
+        Jacobian of y with respect to x. The Op's grad method should return dot(J.T,z).
+        When theano.tensor.grad calls the grad method, it will set z to be the gradient 
+        of the cost C with respect to y. If this op is the only op that acts on x, then
+        dot(J.T,z) is the gradient of C with respect to x. If there are other ops that 
+        act on x, theano.tensor.grad will have to add up the terms of x's gradient 
+        contributed by the other op's grad method.
+        """        
+        z_r = output_gradients[0]
+        z_i = output_gradients[1]
+        
+        # check at least one is not disconnected:
+        if (isinstance(z_r.type, DisconnectedType) and 
+            isinstance(z_i.type, DisconnectedType)):
+            return [DisconnectedType, DisconnectedType]
+        
+        if isinstance(z_r.type, DisconnectedType):
+            print 'z_r using zeros_like'
+            z_r = z_i.zeros_like()
+        
+        if isinstance(z_i.type, DisconnectedType):
+            print 'z_i using zeros_like'
+            z_i = z_r.zeros_like()
+        
+        y = InverseFourierOp()(z_r, z_i)
+        return y

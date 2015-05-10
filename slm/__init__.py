2f7647b3155ba89a194bfe5d4b8c7b276b64cd84
Samuel Denny <samueljdenny@gmail.com>
2015-05-10 13:04:18 +0100

    Tidied up slmOptimisation interface, internal details fully contained in __init__.py

--- slm/__init__.py
+++ slm/__init__.py
@@ -16,43 +16,51 @@ class slmOptimisation(object):
         if profile_s is None:
             profile_s = np.ones((self.n_pixels, self.n_pixels))
             
-        self.profile_s = profile_s.astype(theano.config.floatX)
+        assert profile_s.shape == (self.n_pixels, self.n_pixels), 'profile_s is wrong shape, should be ({n},{n})'.format(n=self.n_pixels)
+        self.profile_s_r = profile_s.real.astype(theano.config.floatX)
+        self.profile_s_i = profile_s.imag.astype(theano.config.floatX)
+        
         self.A0 = A0
         
-        # Set up cost function:
+        # Set zeros matrix:
+        self.zero_frame = np.zeros((2*self.n_pixels, 2*self.n_pixels), dtype=theano.config.floatX)
+        
+        # Phi and its momentum for use in gradient descent with momentum:
         self.phi    = theano.shared(value=initial_phi.astype(theano.config.floatX),
                                     name='phi')
         self.phi_rate = theano.shared(value=np.zeros_like(initial_phi).astype(theano.config.floatX),
                                       name='phi_rate')
-        self.S_i = T.matrix('S_i')
-        self.S_r = T.matrix('S_r')
+        
+        self.S_r = theano.shared(value=self.profile_s_r,
+                                 name='s_r')
+        self.S_i = theano.shared(value=self.profile_s_i,
+                                 name='s_i')
+        self.zero_matrix = theano.shared(value=self.zero_frame,
+                                         name='zero_matrix')
         
         # E_in: (n_pixels**2)
         self.E_in_r = self.A0 * (self.S_r*T.cos(self.phi) - self.S_i*T.sin(self.phi))
         self.E_in_i = self.A0 * (self.S_i*T.cos(self.phi) + self.S_r*T.sin(self.phi))
         
         # E_in padded: (4n_pixels**2)
-        self.zero_matrix = T.matrix('zero')
         idx_0, idx_1 = get_centre_range(self.n_pixels)
         self.E_in_r_pad = T.set_subtensor(self.zero_matrix[idx_0:idx_1,idx_0:idx_1], self.E_in_r)
         self.E_in_i_pad = T.set_subtensor(self.zero_matrix[idx_0:idx_1,idx_0:idx_1], self.E_in_i)
         
         # E_out:
-        # f = theano.function([xr, xi], FourierOp()(xr, xi))
         self.E_out_r, self.E_out_i = fft(self.E_in_r_pad, self.E_in_i_pad)        
         
         # finally, the output intensity:
-        #self.E_out_2 = T.pow(self.E_out_r, 2) + T.pow(self.E_out_i, 2)
         self.E_out_2 = T.add(T.pow(self.E_out_r, 2), T.pow(self.E_out_i, 2))
-
         
-    def calculate(self):
-        # Take the current value of self.phi and generate the reconstruction:
-        if self.intensity_calc is None:
-            self.intensity_calc = theano.function(['S_i', 'S_r'], self.E_out_2)
+    def get_E_out(self):
         
-        return self.intensity_calc([self.profile_s_r, self.profile_s_i])
-
+        f_E_out = theano.function([], [self.E_out_r, self.E_out_i], on_unused_input='warn')
+        return f_E_out
+        
+    def get_intensity(self):
+        f_E_out_2 = theano.function([], self.E_out_2, on_unused_input='warn')
+        return f_E_out_2
 
 def get_centre_range(n):
     # returns the indices to use given an nxn SLM
